a6dd01baabb42f57e5d48b7019a8cef756c34e67
daricat <darina.talk@gmail.com>
2020-03-17 14:45:45 +0300

    feat: add solve of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( array === undefined || array.length == 0 ) {
+    return 0;
+  }
+
+  array.sort(function(a, b) {
+    return a - b;
+  });
+  
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( array === undefined || array.length == 0 ) {
+    return 0;
+  }
+  
+  array.sort(function(a, b) {
+    return b - a;
+  });
+
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array === undefined || array.length == 0 ) {
+    return 0;
+  }
+  
+  let sum = 0;
+  
+  for ( let i = 0; i < array.length; i++ ) {
+    sum += array[i];
+  }
+
+  let average = sum/array.length;
+
+  return average;
 }
+
+
