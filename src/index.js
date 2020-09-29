3dc6a407dcf2c94cfdc981ff449fc7e553b414df
daricat <darina.talk@gmail.com>
2020-09-29 12:47:47 +0300

    feat: update solving

--- src/index.js
+++ src/index.js
@@ -1,41 +1,19 @@
 
 exports.min = function min (array) {
-  if ( array === undefined || array.length == 0 ) {
-    return 0;
-  }
-
-  array.sort(function(a, b) {
-    return a - b;
-  });
-  
+  if ( array === undefined || array.length == 0 ) return 0;
+  array.sort((a, b) => a - b);  
   return array[0];
 }
 
 exports.max = function max (array) {
-  if ( array === undefined || array.length == 0 ) {
-    return 0;
-  }
-  
-  array.sort(function(a, b) {
-    return b - a;
-  });
-
+  if ( array === undefined || array.length == 0 ) return 0;  
+  array.sort((a, b) => b - a);
   return array[0];
 }
 
 exports.avg = function avg (array) {
-  if ( array === undefined || array.length == 0 ) {
-    return 0;
-  }
-  
-  let sum = 0;
-  
-  for ( let i = 0; i < array.length; i++ ) {
-    sum += array[i];
-  }
-
-  let average = sum/array.length;
-
+  if ( array === undefined || array.length == 0 ) return 0;
+  let average = array.reduce((sum, element) => sum + element) / array.length;
   return average;
 }
 
